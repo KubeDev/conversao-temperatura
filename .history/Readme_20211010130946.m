d2aa994f4f69e3382274b5658c53f1e96415ee0f
Your Name <jose.costa@jchelp.com.br>
2021-10-10 13:58:56 -0300

    Primera versão

---
+++ .history/Readme_20211010130946.m
@@ -0,0 +1,39 @@
+# Projeto de subir uma aplicação para docker a partir de uma código fonte
+
+*Read this in other languages: [Portugues-Brasil](README.md)
+
+#### Table of Contents
+
+- [Passo-01](#passo-01)
+- [Passo-02](#passo-02)
+- [Passo-03](#passo-03)
+- [Installation](#installation)
+- [Next steps](#next-steps)
+- [Important notes](#important-notes)
+- [Upgrade Libreswan](#upgrade-libreswan)
+- [Manage VPN users](#manage-vpn-users)
+- [Advanced usage](#advanced-usage)
+
+
+## Passo-1
+
+Construir o docker file 
+
+FROM node:14.17.5
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE 8080
+CMD ["node", "server.js"]
+
+## Passo-2
+Dar o build e construir a imagem docker
+
+docker build -t desafio-1-conversao-temperatura .
+
+## Passo-03 
+
+Enviar projeto para o github
+
+git push --set-upstream 
