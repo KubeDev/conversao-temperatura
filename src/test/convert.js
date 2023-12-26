59cec9988546664b67e54047f5b98d530f4e542d
Renato Deccker <67979866+renatodecker@users.noreply.github.com>
2023-12-26 15:17:16 -0300

    Update convert.js

--- src/test/convert.js
+++ src/test/convert.js
@@ -16,7 +16,7 @@ describe('Conversor de Temperatura', () => {
 
       const resultado = convert.celsiusFahrenheit(55);
 
-      expect(resultado).to.equal(131);
+      expect(resultado).to.equal(130);
       done();
   
     });    
